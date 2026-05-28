ad9ece2f0003f16c7c880e0171ff2a46a9fe332f
Abdelhamidagb <2252496@cegepmontpetit.ca>
2026-05-28 16:22:16 -0400

    QReponseunique correction

--- H26-2P6-TP3-2252496/TP3_Quiz_V6_Materiel/Models/QuestionReponseUnique.cs
+++ H26-2P6-TP3-2252496/TP3_Quiz_V6_Materiel/Models/QuestionReponseUnique.cs
@@ -7,46 +7,49 @@ namespace Models
     public class QuestionReponseUnique : IQuestion
     {
         public string Enonce { get; private set; }
+
         public Categorie Categorie { get; private set; }
+
         public int Points { get; private set; }
 
+        public List<string> ChoixReponses { get; private set; }
+
         public string BonneReponse { get; private set; }
-        public List<string> Options { get; private set; }
 
-        public QuestionReponseUnique(string enonce, Categorie categorie, int points,
-            string bonneReponse, List<string> options)
+        public QuestionReponseUnique(string enonce,
+                                     Categorie categorie,
+                                     int points,
+                                     List<string> choixReponses,
+                                     string bonneReponse)
         {
-            if (options == null)
-                throw new ArgumentNullException(nameof(options));
-
-            if (options.Count < 2)
-                throw new ArgumentException("Pas assez d'options");
-
-            if (!options.Contains(bonneReponse))
-                throw new ArgumentException("Bonne réponse invalide");
-
             if (string.IsNullOrWhiteSpace(enonce))
+                throw new ArgumentException();
+
+            if (points <= 0)
+                throw new ArgumentOutOfRangeException(nameof(points));
+
+            if (choixReponses == null || choixReponses.Count < 2)
+                throw new ArgumentException();
+
+            if (string.IsNullOrWhiteSpace(bonneReponse))
                 throw new ArgumentException();
 
             Enonce = enonce;
             Categorie = categorie;
             Points = points;
+            ChoixReponses = choixReponses;
             BonneReponse = bonneReponse;
-            Options = options;
-        }
-
-        public bool ValiderReponse(string reponse)
-        {
-            if (string.IsNullOrWhiteSpace(reponse))
-                return false;
-
-            return reponse.Trim()
-                .Equals(BonneReponse, StringComparison.OrdinalIgnoreCase);
         }
 
         public double CorrigerReponse(string reponse)
         {
             return ValiderReponse(reponse) ? Points : 0;
+        }
+
+        public bool ValiderReponse(string reponse)
+        {
+            return BonneReponse.Trim().ToLower() ==
+                   reponse.Trim().ToLower();
         }
     }
 }
