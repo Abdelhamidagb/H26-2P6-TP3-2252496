20a54b19a028960c10989d4920cd2505f8def392
Abdelhamidagb <2252496@cegepmontpetit.ca>
2026-05-28 12:39:57 -0400

    QreponseCourtes
    debut du questions reponse

--- H26-2P6-TP3-2252496/TP3_Quiz_V6_Materiel/Models/QuestionReponseCourte.cs
+++ H26-2P6-TP3-2252496/TP3_Quiz_V6_Materiel/Models/QuestionReponseCourte.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Models.Interfaces;
 
 namespace Models
 {
-    internal class QuestionReponseCourte
+    public class QuestionReponseCourte : IQuestion
     {
+        public string Enonce { get; private set; }
+        public Categorie Categorie { get; private set; }
+        public int Points { get; private set; }
+
+        public string BonneReponse { get; private set; }
+
+        public string Indice { get; private set; }
+        public double PenaliteIndice { get; private set; }
+
+        private bool indiceUtilise;
+
+        public QuestionReponseCourte(string enonce, Categorie categorie, int points,
+            string bonneReponse, string indice, double penaliteIndice)
+        {
+            if (string.IsNullOrWhiteSpace(bonneReponse))
+                throw new ArgumentException("Bonne réponse invalide");
+
+            if (string.IsNullOrWhiteSpace(enonce))
+                throw new ArgumentException("Enoncé invalide");
+
+            Enonce = enonce;
+            Categorie = categorie;
+            Points = points;
+            BonneReponse = bonneReponse;
+            Indice = indice;
+            PenaliteIndice = penaliteIndice;
+        }
+
+        public void UtiliserIndice()
+        {
+            indiceUtilise = true;
+        }
+
+        public bool ValiderReponse(string reponse)
+        {
+            if (string.IsNullOrWhiteSpace(reponse))
+                return false;
+
+            return reponse.Trim()
+                .Equals(BonneReponse, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public double CorrigerReponse(string reponse)
+        {
+            if (!ValiderReponse(reponse))
+                return 0;
+
+            return indiceUtilise ? Points * 0.5 : Points;
+        }
     }
 }
